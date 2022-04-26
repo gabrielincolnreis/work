8ac655b479805493f6d92e17c0c425571e2129c9
gabrielincolnzip <gabrielincol@zipdin.com.br>
2022-04-25 21:42:54 -0300

    first carol's commit

--- src/main/java/br/com/alura/forum/controller/AddressController.java
+++ src/main/java/br/com/alura/forum/controller/AddressController.java
@@ -1,10 +1,13 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.AddressDto;
+import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.alura.forum.controller.dto.UserDto;
 import br.com.alura.forum.controller.form.AddressForm;
+import br.com.alura.forum.controller.form.UpdateUserForm;
 import br.com.alura.forum.controller.form.UserForm;
 import br.com.alura.forum.modelo.Address;
+import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.modelo.Usuario;
 import br.com.alura.forum.repository.AddressRepository;
 import br.com.alura.forum.repository.UsuarioRepository;
@@ -22,6 +25,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/address")
@@ -51,6 +55,45 @@ public class AddressController {
 
         URI uri = uriBuilder.path("/address/{id}").buildAndExpand(address.getId()).toUri();
         return ResponseEntity.created(uri).body(new AddressDto(address));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressDto> detail(@PathVariable Long id){
+
+        Optional<Address> optional = addressRepository.findById(id);
+        if(optional.isPresent()){
+            return ResponseEntity.ok(new AddressDto(optional.get()));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    @CacheEvict(value = "listAddress", allEntries = true)
+    public ResponseEntity<AddressDto> update(@PathVariable Long id, @RequestBody @Valid AddressForm form){
+
+        Optional<Address> optional = addressRepository.findById(id);
+        if(optional.isPresent()){
+            Address address = form.update(id, addressRepository);
+            return ResponseEntity.ok(new AddressDto(address));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    @CacheEvict(value = "listAddress", allEntries = true)
+    public ResponseEntity<?> delete(@PathVariable Long id){
+
+        Optional<Address> optional = addressRepository.findById(id);
+        if(optional.isPresent()){
+            addressRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 
